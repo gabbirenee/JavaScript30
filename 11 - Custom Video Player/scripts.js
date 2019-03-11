a98e7bfe69dddbbb627f4f42e113b1f059c6bd7e
Gabbi Forsythe <gforsythe17@gmail.com>
2019-03-11 18:36:27 -0400

    11 - grabbed all the necessary elements from the page

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,11 @@
+/* get all the necessary elements */
+const videoPlayer = document.querySelector(".player")
+//console.log(videoPlayer)
+const video = videoPlayer.querySelector(".viewer")
+//console.log(video)
+const progressBar = player.querySelector('.progress');
+const actualProgress = player.querySelector('.progress__filled');
+const playBtn = player.querySelector('.toggle');
+const skipBtns = player.querySelectorAll('[data-skip]');
+const sliders = player.querySelectorAll('.player__slider');
+
